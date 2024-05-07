ff877db4e6b522dc8c472ea5d26c0d78693c5c3b
Liviu1999 <101752562+Liviu1999@users.noreply.github.com>
2024-05-07 15:57:46 +0200

    Finished blogpage

---
+++ client/Component/Latest.jsx
@@ -0,0 +1,33 @@
+import "./Blogpage.css";
+
+function Latest() {
+  return (
+    <div className="bloc-latest">
+      <h2>Latest</h2>
+      <div className="latest-caroussel">
+        <div className="caroussel-card">
+          <img src="/bascket.png" alt="" />
+          <h3>card title</h3>
+        </div>
+        <div className="caroussel-card">
+          <img src="/bascket.png" alt="" />
+          <h3>card title</h3>
+        </div>
+        <div className="caroussel-card">
+          <img src="/bascket.png" alt="" />
+          <h3>card title</h3>
+        </div>
+        <div className="caroussel-card">
+          <img src="/bascket.png" alt="" />
+          <h3>card title</h3>
+        </div>
+        <div className="caroussel-card">
+          <img src="/bascket.png" alt="" />
+          <h3>card title</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Latest;
