91fbf0e6bdf4afa2c1c41140f34e424205c57a65
Liviu1999 <101752562+Liviu1999@users.noreply.github.com>
2024-05-14 10:25:34 +0200

    Connect backend with frontend

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,51 +1,67 @@
-import React, { useState } from 'react';
-import './Login.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightToBracket, faUser } from '@fortawesome/free-solid-svg-icons'; // Import the envelope icon
+import React, { useState } from "react";
+import "./Login.css";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightToBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons"; // Import the envelope icon
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Your validation and login logic here
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Your validation and login logic here
+    try {
+      const response = await axios.post(
+        "https://sport-blog-app-f99d3e95c99d.herokuapp.com/login",
+        { email, password }
+      );
+      console.log(response.data); // Handle success response
+    } catch (error) {
+      console.error("Registration failed:", error);
+      // Handle error
+    }
+  };
 
-    return (
-        <form>
-        <div className='main'>
-            <div className='whitecontainer'>
-                <h1>Login</h1>
-                <div className='gtsya'>Good to see you again !</div>
-                <div className='input-container'>
-                    <p className='infos'>Your email</p>
-                    <input className='ourinfo'
-                    type="email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    placeholder='E-mail' 
-                    required
-                    />
-                </div>
-                <div className='input-container'>
-                    <p className='infos'>Password</p>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password" 
-                        required
-                    />
-                </div>
-                <button className='submitbutton' type="submit">Login
-                <FontAwesomeIcon icon={faArrowRightToBracket} className="icon" /></button>
-            </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="main">
+        <div className="whitecontainer">
+          <h1>Login</h1>
+          <div className="gtsya">Good to see you again !</div>
+          <div className="input-container">
+            <p className="infos">Your email</p>
+            <input
+              className="ourinfo"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="E-mail"
+              required
+            />
+          </div>
+          <div className="input-container">
+            <p className="infos">Password</p>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button className="submitbutton" type="submit">
+            Login
+            <FontAwesomeIcon icon={faArrowRightToBracket} className="icon" />
+          </button>
         </div>
-        </form> 
-    );
-}
+      </div>
+    </form>
+  );
+};
 
 export default Login;
--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,50 +1,77 @@
-import React, { useState } from 'react';
-import './Register.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightToBracket, faUser } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import "./Register.css";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightToBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Your validation and login logic here
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Your validation and login logic here
+    try {
+      const response = await axios.post(
+        "https://sport-blog-app-f99d3e95c99d.herokuapp.com/register",
+        { email, username, password }
+      );
+      console.log(response.data); // Handle success response
+    } catch (error) {
+      console.error("Registration failed:", error);
+      // Handle error
+    }
+  };
 
-    return (
-        <form>
-        <div className='main'>
-            <div className='whitecontainer'>
-                <h1>Register</h1>
-                <div className='gtsya'>Discover our best plans to post your daily blogs and became famous with us !</div>
-                <div className='input-container'>
-                    <p className='infos'>Your email</p>
-                    <input className='ourinfo'
-                    type="email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    placeholder='E-mail' 
-                    required
-                    />
-                </div>
-                <div className='input-container'>
-                    <p className='infos'>Password</p>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password" 
-                        required
-                    />
-                </div>
-                <button className='submitbutton' type="submit">Register
-                <FontAwesomeIcon icon={faArrowRightToBracket} className="icon" /></button>
-            </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="main">
+        <div className="whitecontainer">
+          <h1>Register</h1>
+          <div className="gtsya">
+            Discover our best plans to post your daily blogs and became famous
+            with us !
+          </div>
+          <div className="input-container">
+            <p className="infos">Your email</p>
+            <input
+              className="ourinfo"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="E-mail"
+              required
+            />
+          </div>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            required
+          />
+          <div className="input-container">
+            <p className="infos">Password</p>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button className="submitbutton" type="submit">
+            Register
+            <FontAwesomeIcon icon={faArrowRightToBracket} className="icon" />
+          </button>
         </div>
-        </form> 
-    );
-}
+      </div>
+    </form>
+  );
+};
 
 export default Register;
