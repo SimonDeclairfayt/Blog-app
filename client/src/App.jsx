dda5ee1836684d6d8b8962ec5c2831f5bbd4ddef
Simon Declairfayt <121344377+SimonDeclairfayt@users.noreply.github.com>
2024-05-21 09:53:47 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,35 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Header from "../Component/Header";
+import Blogpage from "../Component/Blogpage/Blogpage";
+import Home from "../Component/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import BlogPost from "./components/BlogPost/BlogPost";
 import "./App.css";
+import Dashboard from './components/Dashboard/Dashboard';
+import Profile from './components/Profile/Profile';
 
-function App() {
+const App = () => {
   return (
     <>
-      <form action="POST">
-        <input type="text" name="title" placeholder="title" />
-        <input type="text" name="tags" placeholder="tags" />
-        <input type="text" name="content" placeholder="content" />
-        <input type="file" name="picture" />
-        <button type="submit">Click</button>
-      </form>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/blogpost" element={<BlogPost />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/categorys" element={<Categorys />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blogpage" element={<Blogpage />} />
+          <Route path="/latest" element={<Latest />} />
+
+        </Routes>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
